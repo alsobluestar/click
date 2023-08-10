edc44596a7499ce468eb32417560398facc146f5
mac <bluestardotos@gmail.com>
2023-08-11 01:51:11 +0700

    hi

---
+++ src/pages/api/click.ts
@@ -0,0 +1,41 @@
+import { createClient } from 'redis';
+
+export default async function handler(req, res) {
+  const client = createClient(process.env.REDIS_CONNECTION_STRING);
+
+  if (req.method === 'GET') {
+    try {
+      await client.connect();
+      const count = await client.get('count');
+      res.json({ count: parseInt(count) });
+    } catch (error) {
+      console.error("An error occurred:", error);
+      res.status(500).send("Internal Server Error");
+    } finally {
+      client.disconnect();
+    }
+
+  } else if (req.method === 'POST') {
+    try {
+      await client.connect();
+      const count = await client.incr('count');
+      res.json({ count });
+    } catch (error) {
+      console.error("An error occurred:", error);
+      res.status(500).send("Internal Server Error");
+    } finally {
+      client.disconnect();
+    }
+  } else {
+    res.status(405).json({ "error": "Method Not Allowed :(" });
+  }
+}
+
+
+
+
+
+
+
+
+
